21db5453e9ce8efe1b43804b58a7b99df71615dc
AbdullhGaber <abdullh760@yahoo.com>
2025-12-31 23:40:58 +0200

    + Implementing Game Server and PlayerHandler classes

--- src/main/java/com/mycompany/server_gui/network/GameServer.java
+++ src/main/java/com/mycompany/server_gui/network/GameServer.java
@@ -1,26 +1,25 @@
 package com.mycompany.server_gui.network;
 
-import com.mycompany.server_gui.network.PlayerHandler;
+import com.mycompany.server_gui.game.GameManager;
 import com.mycompany.server_gui.utils.OnErrorListener;
-import com.mycompany.server_gui.utils.PlayerSymbol;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.function.Consumer;
+import javafx.application.Platform;
 
 public class GameServer extends Thread {
 
-    private static final ArrayList<PlayerHandler> players = new ArrayList();
-    boolean keepRunning = true;
-    OnErrorListener errorListener;
-    Consumer<Integer> onNewClientConnection;
-    ServerSocket serverSocket;
-    PlayerHandler player;
+    private volatile boolean keepRunning;
+    private OnErrorListener errorListener;
+    private Consumer<Integer> onNewClientConnection;
+    private ServerSocket serverSocket;
 
     public GameServer(int port, OnErrorListener errorListener, Consumer<Integer> onNewClientConnection) {
         this.onNewClientConnection = onNewClientConnection;
         this.errorListener = errorListener;
+        this.keepRunning = true; // IMPORTANT: Initialize true
+
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException ex) {
@@ -32,31 +31,32 @@ public class GameServer extends Thread {
     public void run() {
         while (keepRunning) {
             try {
-                Socket s1 = serverSocket.accept();
-                player = new PlayerHandler(s1, this, PlayerSymbol.X, errorListener);
-                player.start();
-                players.add(player);
-                onNewClientConnection.accept(getNumberOfPlayers());
+          
+                Socket s = serverSocket.accept();
+
+                PlayerHandler newPlayer = new PlayerHandler(s, errorListener);
+                newPlayer.start();
+
+                
+                onNewClientConnection.accept(GameManager.getInstance().getPlayersCount());
+
             } catch (IOException ex) {
                 if (keepRunning) {
-                    errorListener.onError(new IOException("Internal Server Error :" + ex.getLocalizedMessage()));
+                    errorListener.onError(ex);
                 }
             }
         }
     }
 
-    public int getNumberOfPlayers() {
-        return players.size();
-    }
-
     public void closeServer() {
         keepRunning = false;
-        if (serverSocket != null) {
-            try {
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
                 serverSocket.close();
-            } catch (IOException ex) {
-                errorListener.onError(new IOException("Error in closing socket" + ex.getLocalizedMessage()));
             }
+            
+        } catch (IOException ex) {
+            errorListener.onError(ex);
         }
     }
 }
--- src/main/java/com/mycompany/server_gui/network/PlayerHandler.java
+++ src/main/java/com/mycompany/server_gui/network/PlayerHandler.java
@@ -1,5 +1,9 @@
 package com.mycompany.server_gui.network;
 
+import com.mycompany.server_gui.game.GameManager;
+import com.mycompany.server_gui.game.GameRoom;
+import com.mycompany.server_gui.model.Player;
+import com.mycompany.server_gui.model.Player.PlayerStatus;
 import com.mycompany.server_gui.utils.OnErrorListener;
 import com.mycompany.server_gui.utils.PlayerSymbol;
 import java.io.BufferedReader;
@@ -9,19 +13,18 @@ import java.io.PrintStream;
 import java.net.Socket;
 
 public class PlayerHandler extends Thread {
-
-    private Socket socket;
+    private final Socket socket;
     private BufferedReader br;
     private PrintStream ps;
-    private PlayerHandler opponent;
-    private GameServer server;
-    private PlayerSymbol symbol;
-    private OnErrorListener errorListener;
+    private final OnErrorListener errorListener;
 
-    public PlayerHandler(Socket socket, GameServer server, PlayerSymbol symbol, OnErrorListener errorListener) {
+    // Player State
+    private GameRoom currentRoom;
+    private PlayerSymbol symbol;
+    private Player player;
+
+    public PlayerHandler(Socket socket, OnErrorListener errorListener) {
         this.socket = socket;
-        this.symbol = symbol;
-        this.server = server;
         this.errorListener = errorListener;
 
         try {
@@ -29,30 +32,100 @@ public class PlayerHandler extends Thread {
             ps = new PrintStream(socket.getOutputStream());
         } catch (IOException ex) {
             errorListener.onError(ex);
-        } finally {
-            ps.println(symbol);
         }
-    }
-
-    public void setOpponent(PlayerHandler opponent) {
-        this.opponent = opponent;
     }
 
     @Override
     public void run() {
         String msg;
-        
-    }
-    
-    public void setPlayerSymbol(PlayerSymbol sym){symbol = sym;}
-
-    private void closeResources() {
         try {
-            socket.close();
-            ps.close();
-            br.close();
-        } catch (IOException e) {
-            errorListener.onError(new IOException("Error in closing resource" + e.getLocalizedMessage()));
+            while ((msg = br.readLine()) != null) {
+                ServerProtocol.processMessage(msg, this);
+            }
+        } catch (IOException ex) {
+            errorListener.onError(new IOException("Problem in receving message : " + ex.getLocalizedMessage()));
+        } finally {
+            cleanup();
         }
+    }
+
+    public void sendMessage(String msg) {
+        if (ps != null) {
+            ps.println(msg);
+            ps.flush();
+        }
+    }
+
+    private void cleanup() {
+        GameManager.getInstance().removePlayer(this);
+        if (currentRoom != null) {
+            currentRoom.handleDisconnect(this);
+        }
+        closeResources();
+    }
+
+    public void closeResources() {
+        try {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (br != null) {
+                br.close();
+            }
+        } catch (IOException e) {
+            errorListener.onError(new IOException("Error closing resources: " + e.getMessage()));
+        }
+    }
+
+    // --- Getters & Setters ---
+    public String getUsername() {
+        return player.getUsername();
+    }
+
+    public void setUsername(String username) {
+        player.setUsername(username);
+    }
+
+    public int getScore() {
+        return player.getScore();
+    }
+
+    public void setScore(int score) {
+        player.setScore(score);
+    }
+
+    public PlayerStatus getStatus() {
+        return player.getStatus();
+    }
+
+    public void setStatus(PlayerStatus status) {
+        player.setStatus(status);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player p) {
+        this.player = p;
+    }
+
+    public GameRoom getGameRoom() {
+        return currentRoom;
+    }
+
+    public void setGameRoom(GameRoom room) {
+        this.currentRoom = room;
+    }
+
+    public PlayerSymbol getPlayerSymbol() {
+        return symbol;
+    }
+
+    public void setPlayerSymbol(PlayerSymbol symbol) {
+        this.symbol = symbol;
     }
 }
