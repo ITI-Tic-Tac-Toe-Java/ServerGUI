89bde39603f8cc43cae27f203495693ec393bd02
AbdullhGaber <abdullh760@yahoo.com>
2025-12-31 23:37:27 +0200

    + Add DB dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.mycompany.server_gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    requires java.sql;
+    requires derbyclient;
 
     opens com.mycompany.server_gui.controller to javafx.fxml;
     exports com.mycompany.server_gui;
