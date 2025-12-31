5b3aa5f6b56b64198f30f17cec97b5e8210e5e5a
AbdullhGaber <abdullh760@yahoo.com>
2025-12-31 23:44:19 +0200

    + Implementing Game and Player POJO classes

--- src/main/java/com/mycompany/server_gui/model/Game.java
+++ src/main/java/com/mycompany/server_gui/model/Game.java
@@ -10,4 +10,48 @@ package com.mycompany.server_gui.model;
  */
 public class Game {
     
+    private int playerXId;
+    private int playerOId;
+    private int winnerId;
+    private String movesData;
+
+    public Game(int playerXId, int playerOId, int winnerId, String movesData) {
+        this.playerXId = playerXId;
+        this.playerOId = playerOId;
+        this.winnerId = winnerId;
+        this.movesData = movesData;
+    }
+
+    public int getPlayerXId() {
+        return playerXId;
+    }
+
+    public void setPlayerXId(int playerXId) {
+        this.playerXId = playerXId;
+    }
+
+    public int getPlayerOId() {
+        return playerOId;
+    }
+
+    public void setPlayerOId(int playerOId) {
+        this.playerOId = playerOId;
+    }
+
+    public int getWinnerId() {
+        return winnerId;
+    }
+
+    public void setWinnerId(int winnerId) {
+        this.winnerId = winnerId;
+    }
+
+    public String getMovesData() {
+        return movesData;
+    }
+
+    public void setMovesData(String movesData) {
+        this.movesData = movesData;
+    }
+
 }
--- src/main/java/com/mycompany/server_gui/model/Player.java
+++ src/main/java/com/mycompany/server_gui/model/Player.java
@@ -9,5 +9,63 @@ package com.mycompany.server_gui.model;
  * @author DELL
  */
 public class Player {
-    
+
+    private int id;
+    private String username;
+    private String password;
+    private int score;
+    private PlayerStatus status;
+
+    public enum PlayerStatus {OFFLINE,IDLE,PLAYING}
+
+    public Player() {
+    }
+
+    public Player(String username, String password, int score, PlayerStatus status) {
+        this.username = username;
+        this.password = password;
+        this.score = score;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public PlayerStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PlayerStatus status) {
+        this.status = status;
+    }
 }
