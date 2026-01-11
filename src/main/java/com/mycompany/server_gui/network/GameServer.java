bc4d823ec11e6f35dd9c054eec2811d4085ab5b6
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-01-11 13:32:16 +0200

    Handled Server Disconnect Error

--- src/main/java/com/mycompany/server_gui/network/GameServer.java
+++ src/main/java/com/mycompany/server_gui/network/GameServer.java
@@ -8,50 +8,54 @@ import java.net.Socket;
 import java.util.function.Consumer;
 
 public class GameServer extends Thread {
-
+    
+    private PlayerHandler newPlayer;
     private volatile boolean keepRunning;
-
+    
     private Consumer<Integer> onNewClientConnection;
     private ServerSocket serverSocket;
-
+    
     public GameServer(int port, Consumer<Integer> onNewClientConnection) {
         this.onNewClientConnection = onNewClientConnection;
         this.keepRunning = true;
-
+        
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException ex) {
             Functions.showErrorAlert(ex);
         }
     }
-
+    
     @Override
     public void run() {
         while (keepRunning) {
             try {
-
                 Socket s = serverSocket.accept();
-
-                PlayerHandler newPlayer = new PlayerHandler(s);
+                
+                newPlayer = new PlayerHandler(s);
                 newPlayer.start();
-
+                
                 onNewClientConnection.accept(GameManager.getInstance().getPlayersCount());
-
+                
             } catch (IOException ex) {
                 if (keepRunning) {
                     Functions.showErrorAlert(ex);
+                    if (newPlayer != null) {
+                        newPlayer.sendMessage("ERROR:SERVER_DISCONNECT");
+                    }
                 }
             }
         }
     }
-
+    
     public void closeServer() {
         keepRunning = false;
         try {
             if (serverSocket != null && !serverSocket.isClosed()) {
                 serverSocket.close();
+                newPlayer.sendMessage("ERROR:SERVER_DISCONNECT");
             }
-
+            
         } catch (IOException ex) {
             Functions.showErrorAlert(ex);
         }
